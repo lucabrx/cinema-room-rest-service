9fc8602964d1d8dc7b8778b5809f5d750da6b1c3
Luka <lukabrkovic@skiff.com>
2023-12-28 22:50:17 +0100

    [fix] typo

--- src/main/java/org/cinema/cinemaroomrestservice/Seats.java
+++ src/main/java/org/cinema/cinemaroomrestservice/Seats.java
@@ -3,37 +3,36 @@ package org.cinema.cinemaroomrestservice;
 import java.util.List;
 
 public class Seats {
-    private int totalRows, totalColumns;
-    private List<Seat> available_seats;
+    private int rows, columns;
+    private List<Seat> seats;
 
-    public Seats(int total_rows, int total_columns, List<Seat> available_seats) {
-        this.totalRows = total_rows;
-        this.totalColumns = total_columns;
-        this.available_seats = available_seats;
+    public Seats(int rows, int columns, List<Seat> seats) {
+        this.rows = rows;
+        this.columns = columns;
+        this.seats = seats;
     }
 
-    public int getTotalRows() {
-        return totalRows;
+    public int getRows() {
+        return rows;
     }
 
-    public void setTotalRows(int totalRows) {
-        this.totalRows = totalRows;
+    public void setRows(int rows) {
+        this.rows = rows;
     }
 
-    public int getTotalColumns() {
-        return totalColumns;
+    public int getColumns() {
+        return columns;
     }
 
-    public void setTotalColumns(int totalColumns) {
-        this.totalColumns = totalColumns;
+    public void setColumns(int columns) {
+        this.columns = columns;
     }
 
-    public List<Seat> getAvailable_seats() {
-        return available_seats;
+    public List<Seat> getSeats() {
+        return seats;
     }
 
-    public void setAvailable_seats(List<Seat> available_seats) {
-        this.available_seats = available_seats;
+    public void setSeats(List<Seat> seats) {
+        this.seats = seats;
     }
 }
-
